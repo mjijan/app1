38a8a2ec26d7c15e360af606571b4e0b85ff83ea
mohammad jijan <58567704+mjijan@users.noreply.github.com>
2024-03-14 08:48:41 +0300

    Update FetchData.js

--- src/FetchData.js
+++ src/FetchData.js
@@ -38,7 +38,7 @@ function HandleClick ()
   return (
     <div className="App">
     
-    <label className="select1">View API Data:</label>
+    <label className="select1">View API Data: 777</label>
 
     <input type="text" className="select1" onChange={ (e) => { HandleChange(e)} } />
     <button className="select1" onClick={ HandleClick }  >Set new id</button>
